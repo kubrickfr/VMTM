3fdd6a8f382ab423e8161de449a31fd92ac9b20a
François Guerraz <francois@orographic.uk>
2016-02-13 16:39:36 +0000

    Remove dead code

---
+++ virgin.sh
@@ -0,0 +1,102 @@
+#!/bin/ash
+
+tc0=12000
+tc1=6100
+tc2=4300
+
+ping_limit=50000
+
+get_current_tc() {
+uci get qos.wan.upload
+}
+
+set_tc() {
+	uci set qos.wan.upload=$1
+	uci commit qos
+	/etc/init.d/qos reload
+}
+
+get_ping() {
+	gateway=$(ip route 2>/dev/null | grep default | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+	if [ "$gateway" == "" ]; then
+		#Fallback on the route command
+		gateway=$(route -n | grep -E '^0\.0\.0\.0' | cut  -f 10 -d ' ')
+	fi
+	ping -c4 -W1 -w10 $gateway | tail -1 | awk -F '/' '{print $4}' | sed 's/\.//'
+}
+
+is_ts_on() {
+	if [ $(date +%u) -lt 6 ]; then
+		if [ $(date +%H) -ge 16 ] && [ $(date +%H) -lt 23 ]; then
+			echo 1
+			return
+		fi
+	else
+		if [ $(date +%H) -ge 11 ] && [ $(date +%H) -lt 23 ]; then
+			echo 1
+			return
+		fi
+	fi
+	echo 0
+}
+
+
+current_tc=$(get_current_tc)
+
+if [ $(is_ts_on) == 0 ]; then
+	echo "We're out of the traffic shaping period"
+	if [ $current_tc -lt $tc0 ]; then
+		echo "Resetting QOS to ${tc0}kbps"
+		set_tc $tc0
+	fi
+else
+	echo "We're in traffic shaping period"
+	if [ $current_tc -lt $tc0 ]; then
+		echo "We're alreading restricting ourselves"
+		if [ $current_tc -gt $tc2 ]; then
+			echo "We can restrict even more, testing if it's necessary..."
+			avg_ping=$(get_ping)
+			if [ $avg_ping -gt $ping_limit ]; then
+				echo "Ping (${avg_ping}ns) is over the limit, restring more! Switching in TC2=${tc2}kbps"
+				set_tc $tc2
+			else
+				if [ $(( $(date +%M) % 10 )) == 0 ]; then
+					echo "We're fine but can we releave the pressure?"
+					set_tc $tc0
+					sleep 5
+					avg_ping=$(get_ping)
+					if [ $avg_ping -gt $ping_limit ]; then
+						echo "Answer is NO, I guess (ping gets to high: ${avg_ping}ns"
+						set_tc $tc1
+					else
+						echo "Yes! Switching in TC0=${tc0}kbps"
+					fi
+				fi
+			fi
+		else
+			echo "... to the maximum!"
+			if [ $(( $(date +%M) % 10 )) == 0 ]; then
+				echo "But can we releave the pressure?"
+				set_tc $tc1
+				sleep 5
+				avg_ping=$(get_ping)
+				if [ $avg_ping -gt $ping_limit ]; then
+					echo "Answer is NO, I guess (ping gets to high: ${avg_ping}ns"
+					set_tc $tc2
+				else
+					echo "Yes! Switching in TC1=${tc1}kbps"
+				fi
+			fi
+		fi
+	else
+		echo "We're not restricting ourselves, do we need to?"
+		avg_ping=$(get_ping)
+		if [ $avg_ping -gt $ping_limit ]; then
+			echo "Ping (${avg_ping}ns) is over the limit, starting restrictions! Switching in TC1=${tc1}kbps"
+			set_tc $tc1
+		else
+			echo "Nah, it's fine... (ping is ${avg_ping}ns)"
+		fi
+	fi
+fi
+
